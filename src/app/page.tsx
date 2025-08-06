e641aa5a35ab9d7e06ae21f759890d9fc38d9d6f
albertoaraujo <alberto.araujoo@gmail.com>
2025-08-06 19:13:33 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,57 @@
-export default function Home() {
-  return <h1>dfd</h1>;
-}
+import Image from "next/image";
+
+import ProductList from "@/components/common/product-list";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  return (
+    <>
+      <div className="space-y-6">
+        <div className="relative px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+          <Button
+            className="hover: absolute bottom-4 left-1/2 -translate-x-1/2 transform cursor-pointer rounded-3xl bg-white/45 px-7 py-1 text-white hover:bg-white/75 hover:text-gray-700"
+            size="lg"
+          >
+            Comprar
+          </Button>
+        </div>
+
+        <ProductList title="Mais vendidos" products={products} />
+
+        <div className="relative px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+          <Button
+            className="hover: absolute bottom-4 left-1/2 -translate-x-1/2 transform cursor-pointer rounded-3xl bg-white/45 px-7 py-1 text-white hover:bg-white/75 hover:text-gray-700"
+            size="lg"
+          >
+            Comprar
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
